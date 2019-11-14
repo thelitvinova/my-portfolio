23c8266114ea3e5e04d5bf40081edbe6ad2020f4
Aleksandra Litvinova <aleksandra@MacBook-Pro-Aleksandra.local>
2019-11-14 19:23:47 +0700

    Updates

---
+++ precache-manifest.11f2b28c44549173e86c3ced32644df3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199efbb0692993b395e546d0c6107bff",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "2f80248e5243d2d98bb6",
+    "url": "/my-portfolio/static/css/main.512e237e.chunk.css"
+  },
+  {
+    "revision": "5df785cd3252ae338444",
+    "url": "/my-portfolio/static/js/2.89931415.chunk.js"
+  },
+  {
+    "revision": "2f80248e5243d2d98bb6",
+    "url": "/my-portfolio/static/js/main.79b2ecf8.chunk.js"
+  },
+  {
+    "revision": "0ed6c73f5c9489d60323",
+    "url": "/my-portfolio/static/js/runtime-main.e8115476.js"
+  },
+  {
+    "revision": "87710d1ee92e2619d5bc5382b0130916",
+    "url": "/my-portfolio/static/media/github.87710d1e.png"
+  },
+  {
+    "revision": "4768b66473beb53c788c63ebef5467d7",
+    "url": "/my-portfolio/static/media/one.4768b664.jpg"
+  },
+  {
+    "revision": "a269e6ce2c06f6885151fff697ff1836",
+    "url": "/my-portfolio/static/media/onepro.a269e6ce.jpg"
+  },
+  {
+    "revision": "3b85efd2052872dec5a39ef1ebef6f9a",
+    "url": "/my-portfolio/static/media/telegram.3b85efd2.png"
+  },
+  {
+    "revision": "6764236a6e0d86a262e2e2402ddad09b",
+    "url": "/my-portfolio/static/media/three.6764236a.jpg"
+  },
+  {
+    "revision": "6d1f98f8bef033a5c4bf9143f15ac4e3",
+    "url": "/my-portfolio/static/media/threepro.6d1f98f8.jpg"
+  },
+  {
+    "revision": "e679c70f99a9a8830555af621716f039",
+    "url": "/my-portfolio/static/media/two.e679c70f.jpg"
+  },
+  {
+    "revision": "a3a67983513889daad7d4cc7573fedef",
+    "url": "/my-portfolio/static/media/twopro.a3a67983.jpg"
+  },
+  {
+    "revision": "a25c9c945be77dfc71a1f5b5edf3d498",
+    "url": "/my-portfolio/static/media/whatsapp.a25c9c94.png"
+  }
+]);
